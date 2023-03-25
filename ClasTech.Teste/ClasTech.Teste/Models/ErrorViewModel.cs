2b23b2d323e90544fd10c4ab5dff10728cac2251
João Manoel Araujo Silva <joao_manuel@live.com>
2023-03-25 09:38:51 -0300

    Salvar no git

---
+++ ClasTech.Teste/ClasTech.Teste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClasTech.Teste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
